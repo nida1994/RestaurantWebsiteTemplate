e588b1be9117a29a43205c976a34f01a5d9bc6f9
Nida Ali <nida.b94@gmail.com>
2021-12-05 13:27:51 -0500

    Add files via upload

--- source-code/index.php
+++ source-code/index.php
@@ -1,1 +1,70 @@
+<?php include 'header.php'; ?>
 
+    <section class="restaurant-about-us-section" id="restaurant-about-us-section">
+        <div class="container">
+            <div class="row mt-5 about-us-box">
+                <div class="col-md-6 about-us-image">
+                    <img src="images/aboutus_image.jpg" class="img-fluid">
+                </div>
+                <div class="col-md-6 about-us-content">
+                    <h2>Welcome</h2>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="slider-area">
+            <div class="container">
+                <h2>What Our Customers Say</h2>
+                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <div class="img-container">
+                                <img class="d-block w-100" src="images/person1.png" alt="First slide">
+                            </div> 
+                             <div class="carousel-caption d-md-block">
+                                <h3>Sibgha Butt<h3>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, molestias architecto id porro ea soluta totam laborum, repellendus quos, ex labore nam doloribus molestiae sapiente ratione aut est ab provident!</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <div class="img-container">
+                                <img class="d-block w-100" src="images/person2.png" alt="Second slide">
+                            </div>
+                            <div class="carousel-caption d-md-block">
+                                <h3>Waleed Ali<h3>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, molestias architecto id porro ea soluta totam laborum, repellendus quos, ex labore nam doloribus molestiae sapiente ratione aut est ab provident!</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <div class="img-container">
+                                <img class="d-block w-100" src="images/person3.png" alt="Third slide">
+                            </div>
+                            <div class="carousel-caption d-md-block">
+                                <h3>Sohail Butt<h3>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, molestias architecto id porro ea soluta totam laborum, repellendus quos, ex labore nam doloribus molestiae sapiente ratione aut est ab provident!</p>
+                            </div>
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>    
+        
+<?php include 'footer.php'; ?>
+       
